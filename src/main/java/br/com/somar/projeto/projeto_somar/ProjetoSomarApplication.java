beb84b7aacf8f11438971f6303a503f3b41d2178
etmamate <maateus.cristo132@gmail.com>
2025-08-21 20:43:42 -0300

    estrutura

---
+++ src/main/java/br/com/somar/projeto/projeto_somar/ProjetoSomarApplication.java
@@ -0,0 +1,13 @@
+package br.com.somar.projeto.projeto_somar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSomarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSomarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/somar/projeto/projeto_somar/ProjetoSomarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.somar.projeto.projeto_somar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSomarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
